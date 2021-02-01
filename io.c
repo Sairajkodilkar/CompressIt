db52e9e07052f5b479bf89e36c54a383ad3997fb
Sairaj Kodilkar <skodilkar7@gmali.com>
2021-02-01 22:14:41 +0530

    directory structure updated

---
+++ io.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "io.h"
+/* Handles file input output */
+
+
+File open_text_file(char *name){
+	/* opens the text file for reading */
+	/* store its attributes in File struct */
+	/* return that file struct */
+}
+
+char read_text_file(File *input){
+	/* performs low level reading */
+	/* returns charter by character */
+}
+
+void write_bit(File *output, bit in_bit){
+	/* writes to the file bit by bit */
+}
+
+bit readbit(File *input){
+	/* Reads file bit by bit */
+}
+
+
+
