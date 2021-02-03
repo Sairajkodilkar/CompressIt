b9a8bc797096b5f8bfb7ca969cbaa5e40ca86216
Sairaj Kodilkar <skodilkar7@gmali.com>
2021-02-03 22:10:11 +0530

    Directory struct improved

---
+++ include/huffman.h
@@ -0,0 +1,14 @@
+//reading from file: 
+// read file and count frequency
+
+// Huffman :
+// make the heap of that frequency TODO: write heap ADT for node
+// extrating node from heap build huffman tree
+// count the lenght of each pattern
+// count the occurences of each length
+// convert pattern into canonical huffman code
+
+//writing to file:
+//write header
+//write the occurences of each length 
+//write end of the file
